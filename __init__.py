9298b63427a3a279ea01d3e2493c14deb6f0606f
Viktor Kamp <info@viktorkamp.de>
2026-04-17 09:42:53 +0200

    Update __init__.py
    Updated version number to 1.0.1

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ import bpy
 bl_info = {
     "name": "Unit to pixel converter",
     "author": "Viktor Kamp",
-    "version": (1, 0, 0),
+    "version": (1, 0, 1),
     "blender": (4, 2, 0),
     "location": "Properties > Output > Unit to pixel",
     "description": "Calculates render resolution from different units, including inch, centimeter or millimeter.",
